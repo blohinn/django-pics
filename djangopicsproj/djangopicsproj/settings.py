b7a28992aeaab8b50d06b918db897bb6925d2db6
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-08-03 13:51:26 +0300

    Settings for custom templates for allauth.

--- djangopicsproj/djangopicsproj/settings.py
+++ djangopicsproj/djangopicsproj/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'djangopicsproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),
+                 os.path.join(BASE_DIR, 'profileapp', 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
