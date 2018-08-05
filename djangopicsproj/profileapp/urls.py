eded2f1ec58d2718072af9038c41b74dba544bfb
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-08-05 09:31:30 +0300

    Added nav-bar.

--- djangopicsproj/profileapp/urls.py
+++ djangopicsproj/profileapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<username>[\w\-]+)$', views.profile),
+    url(r'^(?P<username>[\w\-]+)$', views.profile, name='view_profile'),
     url(r'^profile/edit$', views.edit_profile, name='edit'),
 ]
