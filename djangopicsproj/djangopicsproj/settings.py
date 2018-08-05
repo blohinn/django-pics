34a99d87d14b14bab37ebea38ac1cc42ed760a6e
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-08-05 15:39:49 +0300

    Static.

--- djangopicsproj/djangopicsproj/settings.py
+++ djangopicsproj/djangopicsproj/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 SITE_ID = 1
